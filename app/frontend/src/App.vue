7eb2b157a5818df9a9fd7f77aa9bc337ed01c681
Loïc HUANG <huang.loic2004@gmail.com>
2026-04-14 14:13:05 +0200

    Installation de Vue+Vite

---
+++ app/frontend/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="app">
+    <h1 class="title">Mon application Web</h1>
+
+    <router-view />
+  </div>
+</template>
+
+<style scoped>
+.title {
+  text-align: center;
+  font-size: 2.5rem;
+  font-weight: bold;
+  margin: 20px 0;
+}
+</style>
